cfcea3eb0d4081063c634b1ab71e85aeaca9ed9a
RaulDdrt <rauldiazdrt@gmail.com>
2023-04-01 19:03:13 +0100

    segundo commit

---
+++ testProfessional.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prof_1 = require("./prof");
+var professional1 = new prof_1.Profesional("Ridley Scott", 85, 1.74, 80, false, "Inglesa", 1, "Director");
+professional1.showAll();
---
+++ testProfessional.ts
@@ -0,0 +1,5 @@
+import { Profesional } from "./prof";
+
+let professional1 = new Profesional ("Ridley Scott", 85, 1.74, 80, false, "Inglesa", 1, "Director")
+
+professional1.showAll()
